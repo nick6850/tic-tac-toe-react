eae75a1b5fe44c75845837d3adf3ab19948d6041
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-11-07 00:24:16 +0300

    Made Tic Tac Toe game functionality work and finished the project

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "./Square";
+import patterns from "./patterns";
 
 function Board() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [playerValue, setPlayerValue] = useState("O");
+  const [playerValue, setPlayerValue] = useState("X");
+
+  useEffect(() => {
+    patterns.forEach((pattern) => {
+      if (
+        board[pattern[0]] &&
+        board[pattern[1]] &&
+        board[pattern[2]] &&
+        board[pattern[0]] == board[pattern[1]] &&
+        board[pattern[1]] == board[pattern[2]] &&
+        board[pattern[0]] == board[pattern[2]]
+      ) {
+        alert(`${board[pattern[0]]} won!`);
+        setBoard(["", "", "", "", "", "", "", "", ""]);
+      }
+    });
+
+    if (board.filter((square) => square === "").length === 0) {
+      alert(`That's a tie!`);
+      setBoard(["", "", "", "", "", "", "", "", ""]);
+    }
+  }, [board]);
 
   function handleClick(chosenIndex) {
     setBoard((prev) =>
